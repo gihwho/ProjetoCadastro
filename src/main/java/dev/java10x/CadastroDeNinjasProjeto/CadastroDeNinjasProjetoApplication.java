189f0409c6dbeb4ac5e086f510300e3909112690
Giovanna Borges <giovannaborgesmassi@gmail.com>
2026-01-22 18:14:46 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeNinjasProjeto/CadastroDeNinjasProjetoApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjasProjeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjasProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastroDeNinjasProjeto/CadastroDeNinjasProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjasProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
