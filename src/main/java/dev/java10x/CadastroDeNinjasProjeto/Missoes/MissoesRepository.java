f1b206476ce4dad92428afb736a9ddaf51d3c0c6
Giovanna Borges <giovannaborgesmassi@gmail.com>
2026-02-18 16:21:26 -0300

    refactor: comment sobre JPA

--- src/main/java/dev/java10x/CadastroDeNinjasProjeto/Missoes/MissoesRepository.java
+++ src/main/java/dev/java10x/CadastroDeNinjasProjeto/Missoes/MissoesRepository.java
@@ -2,5 +2,7 @@ package dev.java10x.CadastroDeNinjasProjeto.Missoes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//JPA escaneia o MissoesModel e passa o tipo de ID contido nele, basicamente, para
+//conectar classes da aplicação a tabelas do banco de dados relacional
 public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
 }
